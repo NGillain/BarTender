717a90cc76813fd68930e07eef888bc2c1a341db
mdelandmeter <mathieu.delandmeter@student.uclouvain.be>
2015-03-04 12:23:31 +0100

    Create Script.sh

---
+++ Script.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+rm commandes.sql
+
+while read line
+do
+	nom=`echo "$line" | cut -d ";" -f 2`
+	prixAchat=`echo "$line" | cut -d ";" -f 3`
+	prixVente=`echo "$line" | cut -d ";" -f 4`
+	photo=`echo "$line" | cut -d ";" -f 6`
+	stock=`echo "$line" | cut -d ";" -f 7`
+	max=`echo "$line" | cut -d ";" -f 8`
+	seuil=`echo "$line" | cut -d ";" -f 9`
+	allergies=`echo "$line" | cut -d ";" -f 10`
+	description=`echo "$line" | cut -d ";" -f 11`
+
+
+	if [[ -z "$nom" ]]
+	then
+		nom="null"
+	fi
+	if [[ -z "$prixAchat" ]]
+	then
+		prixAchat="null"
+	fi	
+	if [[ -z "$prixVente" ]]
+	then
+		prixVente="null"
+	fi
+	if [[ -z "$photo" ]]
+	then
+		photo="null"
+	fi
+	if [[ -z "$stock" ]]
+	then
+		stock="null"
+	fi
+	if [[ -z "$max" ]]
+	then
+		max="null"
+	fi
+	if [[ -z "$seuil" ]]
+	then
+		seuil="null"
+	fi
+	if [[ -z "$description" ]]
+	then
+		description="null"
+	fi
+
+	prixAchat=`echo "$prixAchat" | sed 's/,/\./g'`
+	prixVente=`echo "$prixVente" | sed 's/,/\./g'`
+	echo "INSERT INTO boissons VALUES ($nom, $prixAchat, $prixVente, '$photo', $stock, $max, $seuil, '$description')" >> commandes.sql
+done < boissons.cvs
