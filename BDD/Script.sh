bb0204b19608b752cd6ceb410bde8c03097984d0
tmstyle96 <thomas.marissal96@gmail.com>
2015-04-22 11:46:21 +0200

    lol

--- BDD/Script.sh
+++ BDD/Script.sh
@@ -52,5 +52,5 @@ do
 	types='null'
 	prixAchat=`echo "$prixAchat" | sed 's/,/\./g'`
 	prixVente=`echo "$prixVente" | sed 's/,/\./g'`
-	echo "INSERT INTO Boissons VALUES ('$nom', $prixAchat, $prixVente, '$description', '$photo', $types, $stock, $seuil, $max);" >> commandes.sql
+	echo "INSERT INTO Boisson VALUES ('$nom', $prixAchat, $prixVente, '$description', '$photo', $types, $stock, $seuil, $max);" >> commandes.sql
 done < boissons.cvs
